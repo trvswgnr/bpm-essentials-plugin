8d27efcf839885ac71dd9ab9aec65210aad59b32
trav <trvswgnr@gmail.com>
2019-01-23 18:48:10 -0500

    added remove gutenberg function

---
+++ functions/remove-gutenberg.php
@@ -0,0 +1,16 @@
+<?php
+/**
+	* Remove Gutenberg Editor
+	*/
+// disable for posts
+add_filter('use_block_editor_for_post', '__return_false', 10);
+
+// disable for post types
+add_filter('use_block_editor_for_post_type', '__return_false', 10);
+
+// disable gutenberg style in frontend
+function wps_deregister_styles() {
+	wp_dequeue_style( 'wp-block-library' );
+}
+add_action( 'wp_print_styles', 'wps_deregister_styles', 100 );
+?>
