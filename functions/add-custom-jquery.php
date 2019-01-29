c2de1cfcfdc5b14a9837535d4be4ca15d22fffdf
trav <trvswgnr@gmail.com>
2019-01-29 17:25:02 -0500

    more verbose php comments

--- functions/add-custom-jquery.php
+++ functions/add-custom-jquery.php
@@ -1,8 +1,18 @@
 <?php
-// custom jQuery
-function custom_jquery() {
-  wp_deregister_script('jquery');
-  wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', array(), '3.2.1', true);
+/**
+	* Custom jQuery
+	* Replaces the old, outdated WP jQuery
+	*/
+function bpm_custom_jquery() {
+  wp_deregister_script('jquery'); // deregister WP default jQuery
+  wp_enqueue_script( // add updated version of jQuery
+		'jquery',
+		'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js',
+		array(), // no dependencies
+		'3.2.1',
+		true // load in footer
+		/* @NOTE: if any js in <head> has jquery as a dependency then this jquery will be bumped to the <head> regardless of the in_footer parameter */
+	);
 }
-add_action('wp_enqueue_scripts', 'custom_jquery');
+add_action('wp_enqueue_scripts', 'bpm_custom_jquery');
 ?>
