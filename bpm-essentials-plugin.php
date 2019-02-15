76f4cebdb76d7d5027f609193421e8ff736e0d0d
trav <trvswgnr@gmail.com>
2019-02-14 21:43:47 -0500

    reformat

--- bpm-essentials-plugin.php
+++ bpm-essentials-plugin.php
@@ -7,37 +7,28 @@ Version: 1.0
 Author: Travis Wagner
 Author URI: http://travisaw.com
 */
+
+/** Simple helper to include function partials */
 function bpm_include($filename) {
-  return require_once( dirname( __FILE__ ) . '/functions/' . $filename . '.php' );
-	// helper functions to include partials
+  return require_once( dirname( __FILE__ ) . '/' . $filename . '.php' );
 }
 
-if (get_option('custom_jquery')) {
-	bpm_include('add-custom-jquery');
-}
+if (get_option('custom_jquery')) bpm_include('functions/add-custom-jquery');
 
-if (get_option('svg_upload_support')) {
-	bpm_include('add-svg-upload-support');
-}
+if (get_option('svg_upload_support')) bpm_include('functions/add-svg-upload-support');
 
-if (get_option('remove_head_junk')) {
-	bpm_include('remove-head-junk');
-}
+if (get_option('remove_head_junk')) bpm_include('functions/remove-head-junk');
 
-if (get_option('remove_autoformatting')) {
-	bpm_include('remove-autoformatting');
-}
+if (get_option('remove_autoformatting')) bpm_include('functions/remove-autoformatting');
 
-if (get_option('remove_gutenberg')) {
-	bpm_include('remove-gutenberg');
-}
+if (get_option('remove_gutenberg')) bpm_include('functions/remove-gutenberg');
 
-if (get_option('slick_carousel')) {
-	bpm_include('add-slick-carousel');
-}
+if (get_option('slick_carousel')) bpm_include('functions/add-slick-carousel');
 
-if (get_option('utility')) {
-	bpm_include('add-utility');
-}
+if (get_option('utility')) bpm_include('functions/add-utility');
 
-bpm_include('add-options-page');
+if (get_option('add_select2')) bpm_include('functions/add-select2');
+
+bpm_include('functions/add-options-page');
+
+bpm_include('shortcodes/cta');
